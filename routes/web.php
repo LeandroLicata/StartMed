e63e6706967c57fde42b866b88ad717f5bb46b4c
Leandro Licata <leandrolicata1@gmail.com>
2026-08-06 00:39:35 -0300

    Correr Pint sobre routes/web.php: BOM y una indentación que quedaron sueltas
    Sin relación con el contenido de las rutas: un BOM al inicio del archivo
    y el bloque de agenda/historial del cirujano con doble indentación,
    ambos de antes de esta serie de cambios.

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\AuditoriaController;
@@ -61,16 +61,16 @@ Route::middleware('auth')->group(function () {
         ->middleware('rol:Cirujano')
         ->name('cirujano.historial');
 
-        Route::get('/cirujano/agenda', [CirujanoController::class, 'agenda'])
-            ->middleware('rol:Cirujano')
-            ->name('cirujano.agenda');
-        Route::get('/cirujano/agenda/{fecha}', [CirujanoController::class, 'agendaDia'])
-            ->middleware('rol:Cirujano')
-            ->name('cirujano.agenda.dia');
-            // >>> NUEVA RUTA PARA VER EL DETALLE DE UNA CIRUGÍA <<<
-        Route::get('/cirujano/cirugias/{cirugia}', [CirujanoController::class, 'detalle'])
-            ->middleware('rol:Cirujano')
-            ->name('cirujano.cirugias.detalle');
+    Route::get('/cirujano/agenda', [CirujanoController::class, 'agenda'])
+        ->middleware('rol:Cirujano')
+        ->name('cirujano.agenda');
+    Route::get('/cirujano/agenda/{fecha}', [CirujanoController::class, 'agendaDia'])
+        ->middleware('rol:Cirujano')
+        ->name('cirujano.agenda.dia');
+    // >>> NUEVA RUTA PARA VER EL DETALLE DE UNA CIRUGÍA <<<
+    Route::get('/cirujano/cirugias/{cirugia}', [CirujanoController::class, 'detalle'])
+        ->middleware('rol:Cirujano')
+        ->name('cirujano.cirugias.detalle');
 
     Route::get('/anestesista', [AnestesistaController::class, 'index'])
         ->middleware('rol:Anestesista')
